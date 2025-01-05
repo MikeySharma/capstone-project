08a164bc01f1afaec2a9748d174bd2c64498a374
Satyam Sah <satnarayan5166@gmail.com>
2025-01-05 06:13:06 +0545

    register page error fix

--- frontend/components/Register.tsx
+++ frontend/components/Register.tsx
@@ -6,6 +6,7 @@ import { authService } from '@/services/authService';
 import { toast } from 'react-toastify';
 import { AuthCard } from './shared/AuthCard';
 import Image from 'next/image';
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
 export default function Register() {
   const router = useRouter();
@@ -84,10 +85,7 @@ export default function Register() {
               className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowPasswords({ ...showPasswords, password: !showPasswords.password })}
             >
-              <Image src={showPasswords.password ? "/icons/HidePassword.png" : "/icons/showPassword.png"} 
-                     alt={showPasswords.password ? "Hide Password" : "Show Password"} 
-                     width={16} 
-                     height={16} />
+              <i className='w-8 h-8'>{showPasswords.password ? <IoEyeOff/> : <IoEye/>}</i>
             </button>
           </div>
 
@@ -105,10 +103,7 @@ export default function Register() {
               className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowPasswords({ ...showPasswords, confirmPassword: !showPasswords.confirmPassword })}
             >
-              <Image src={showPasswords.confirmPassword ? "/icons/HidePassword.png" : "/icons/showPassword.png"} 
-                     alt={showPasswords.confirmPassword ? "Hide Password" : "Show Password"} 
-                     width={16} 
-                     height={16} />
+              <i className='w-8 h-8'>{showPasswords.password ? <IoEyeOff/> : <IoEye/>}</i>
             </button>
           </div>
 
