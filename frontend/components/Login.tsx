6108110cf2f27b846905ede583e8ccc770cf5af2
Satyam Sah <satnarayan5166@gmail.com>
2025-01-05 06:19:15 +0545

    icon added

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -7,6 +7,8 @@ import { toast } from 'react-toastify';
 import Cookies from 'js-cookie'
 import { AuthCard } from './shared/AuthCard';
 import Image from 'next/image';
+import { IoEye } from 'react-icons/io5';
+import { IoEyeOff } from 'react-icons/io5';
 
 export default function Login() {
   const router = useRouter();
@@ -87,12 +89,7 @@ export default function Login() {
               className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowPassword(!showPassword)}
             >
-              <Image 
-                src={showPassword ? "/icons/HidePassword.png" : "/icons/showPassword.png"} 
-                alt={showPassword ? "Hide Password" : "Show Password"} 
-                width={16} 
-                height={16} 
-              />
+              <i className='w-8 h-8'>{showPassword ? <IoEyeOff/> : <IoEye/>}</i>
             </button>
           </div>
 
--- frontend/components/Register.tsx
+++ frontend/components/Register.tsx
@@ -103,7 +103,7 @@ export default function Register() {
               className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowPasswords({ ...showPasswords, confirmPassword: !showPasswords.confirmPassword })}
             >
-              <i className='w-8 h-8'>{showPasswords.password ? <IoEyeOff/> : <IoEye/>}</i>
+              <i className='w-8 h-8'>{showPasswords.confirmPassword ? <IoEyeOff/> : <IoEye/>}</i>
             </button>
           </div>
 
